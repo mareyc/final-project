046c094f7b5d4fb7857dadda6f7b22493a949efe
mareyc <chao.mary@gmail.com>
2016-09-29 12:17:29 -0700

    testing

--- js/script.js
+++ js/script.js
@@ -1,41 +1,93 @@
 $(document).ready(function(){
+	event.preventDefault();
+	// CAROUSEL
+	// var pauseInterval = false;
 
-	var interval = window.setInterval(rotateSlides, 3000)
+	// var interval = window.setInterval(rotateSlides, 3000)
 
-	function rotateSlides() {
-		var $firstSlide = $('#carousel').find('div:first')
-		var width = $firstSlide.width()
+	// function rotateSlides(){
+	// 	var $firstSlide = $('.carousel').find('div:first');
+	// 	var width = $firstSlide.width();
 
-		$firstSlide.animate({marginLeft: -width}, 1000, function(){
-		var $lastSlide = $('#carousel').find('div:last')
-		$lastSlide.after($firstSlide);
-		$firstSlide.css({marginLeft: 0});
-		})
+	// 	$firstSlide.animate({marginLeft: -width}, 1000, function(){
+	// 		var $lastSlide = $('.carousel').find('div:last')
+	// 		$lastSlide.after($firstSlide);
+	// 		$firstSlide.css({marginLeft: 0})
+	// 	});
+	// }
+
+	// $('#left-arrow').click(previousSlide);
+	// $('#right-arrow').click(nextSlide);
+	// $('#left-arrow2').click(previousSlide);
+	// $('#right-arrow2').click(nextSlide);
+	// $('#left-arrow3').click(previousSlide);
+	// $('#right-arrow3').click(nextSlide);
+	// $('.slide-image').click(nextSlide);
+
+	$('.left-arrow').click(previousSlide);
+	$('.right-arrow').click(nextSlide);
+	$('.slide-image').click(nextSlide);
+
+
+	function previousSlide(){
+		// window.clearInterval(interval);
+
+		//grabs the id for the carousel in the parent elemtn
+		// var carouselIdValue = $(this).attr('id');
+		// console.log(carouselIdValue);
+
+		// var carouselIdValue = $(this).parent('id');
+		var carouselIdValue = '#' + ($(this).parent().attr('id'));
+		console.log(carouselIdValue);
+
+		var $currentSlide = $(carouselIdValue).find('div:first');
+		var width = $currentSlide.width();
+		var $previousSlide = $(carouselIdValue).find('div:last')
+		$previousSlide.css({marginLeft: -width})
+		$currentSlide.before($previousSlide);
+		// $previousSlide.animate({marginLeft: 0}, 1000, linear, function(){
+		// 	interval = window.setInterval(rotateSlides, 3000);
+		// });
+		$previousSlide.animate({marginLeft: 0}, 500, 'linear');
+
 	}
 
-	// $(".read-more").click(showMore);
-	// $(".read-less").click(showLess);
-	// $(".learn-more").click(showMoreLearn);
+	function nextSlide(){
+		var carouselIdValue = '#' + ($(this).parent().attr('id'));
+		// window.clearInterval(interval);
+		var $currentSlide = $(carouselIdValue).find('div:first');
+		var width = $currentSlide.width();
+		$currentSlide.animate({marginLeft: -width}, 500, 'linear', function(){
+			var $lastSlide = $(carouselIdValue).find('div:last')
+			$lastSlide.after($currentSlide);
+			$currentSlide.css({marginLeft: 0})
+			// interval = window.setInterval(rotateSlides, 3000);
+		});
+	}	
+
+	// READ MORE
+	$(".read-more").click(showMore);
 	
-	// $("img").mouseover(function(){
- //    	$("img").css("border", "5px solid #033048");
- //    });
-
- //    $("img").mouseout(function(){
- //    	$("img").css("border", "none");
- //    });
-
- //    $(".button").click(function(){
-	// 	event.preventDefault();
- // 		alert( "We're Not Ready For Sign-Ups...Yet." );
- // 	});
     	
-	// function showMore(){
-	// 	event.preventDefault();
-	// 	$("#show-this-on-click").slideDown();
-	// 	$(".read-more").hide();
-	// 	$(".read-less").show();
-	// 	}
+	function showMore(){
+		event.preventDefault();
+		$(".show-this-on-click").toggle(500);
+		$(".read-more").hide();
+		// $(".read-less").show();
+		}
+
+	// HOMEPAGE NAVIGATION
+	//.fadeIn()
+	$('#button-homepage-nav').click(function(){
+		event.preventDefault();
+		// alert( 'Handler for .click() called.' );
+		// console.log('testing');
+		// $('section').removeClass('hide');
+		$('#homepage-show-buttons').fadeIn();
+		// $('section:first').addClass('hide');
+		$('#button-homepage-nav').hide();
+	});
+
 
 	// function showLess(){
 	// 	event.preventDefault();
@@ -44,10 +96,17 @@ $(document).ready(function(){
 	// 	$(".read-more").show();
 	// 	}
 
-	// function showMoreLearn(){
-	// 	event.preventDefault();
-	// 	$("#learn-more-text").slideDown();
-	// 	$(".learn-more").hide();
-	// 	}
+	// 	$("img").mouseover(function(){
+ //    	$("img").css("border", "5px solid #033048");
+ //    	});
 
-})
+ //    $("img").mouseout(function(){
+ //    	$("img").css("border", "none");
+ //    	});
+
+ //    $(".button").click(function(){
+	// 	event.preventDefault();
+ // 		alert( "We're Not Ready For Sign-Ups...Yet." );
+ // 		});
+
+});
